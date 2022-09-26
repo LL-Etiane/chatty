5be9965eee7555b2d0dcf2b83a87e75c36f27419
LL-Etiane <lukongleinyuyetiane@gmail.com>
2022-09-26 02:31:09 +0100

    site primary and secondary color added

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -5,6 +5,7 @@
   <main>
     <div class="grid h-screen place-items-center">
       <h1 class="text-4xl">Chatty</h1>
+      <button class="p-1 bg-primary text-white rounded-md">button</button>
     </div>
   </main>
 </template>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,12 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary": "#00ADB5",
+        "secondary": "#222831"
+      }
+    },
   },
   plugins: [],
 }
