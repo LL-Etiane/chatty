7c75b4507e9ee3deab7e0e90286feffb5b41ff14
LL-Etiane <lukongleinyuyetiane@gmail.com>
2022-10-01 01:55:02 +0100

    post component added

---
+++ frontend/src/components/PostComponent.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="bg-white my-2 p-2">
+      <div class="flex text-sm space-x-4">
+        <p class="font-bold">etiane</p>
+        <p class="text-slate-400">Posted on </p>
+      </div>
+      <h1 class="font-bold">Post title</h1>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem autem nulla deleniti suscipit dolor nemo cum quisquam molestias porro earum, soluta repellendus sit doloribus dolores eaque numquam. Magnam, hic amet.... <button class="bg-primary p-1 text-sm rounded-md text-white">Read more</button></p>
+    </div>
+</template>
