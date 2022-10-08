1b861b81d2f376bfa7f2ad8b094ebf04575038bb
LL-Etiane <lukongleinyuyetiane@gmail.com>
2022-10-08 12:02:48 +0100

    Register page added

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import LoginView from '../views/Login.vue'
+import RegisterView from '../views/Register.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,6 +16,11 @@ const router = createRouter({
       name: 'login',
       component: LoginView
     },
+    {
+      path: '/register',
+      name: 'register',
+      component: RegisterView
+    }
   ]
 })
 
---
+++ frontend/src/views/Register.vue
@@ -0,0 +1,25 @@
+<script setup>
+</script>
+
+<template>
+    <main class="h-screen grid place-items-center">
+        <div class="bg-white py-2 px-4 rounded-sm shadow-md">
+            <h1 class="my-2">Create an account</h1>
+            <form action="">
+                <div class="form-group">
+                    <label for="email">Email</label>
+                    <input type="email" name="email" id="email" class="form-input w-full">
+                </div>
+                <div class="form-group">
+                    <label for="password">Password</label>
+                    <input type="password" name="password" id="password" class="w-full">
+                </div>
+                <div class="form-group">
+                    <label for="passwordConfirm">Confirm your password</label>
+                    <input type="password" name="passwordConfirm" id="passwordConfirm" class="w-full">
+                </div>
+                <input type="submit" value="Register" class="bg-primary p-2 my-2 rounded-sm text-white font-bold">
+            </form>
+        </div>
+    </main>
+</template>
