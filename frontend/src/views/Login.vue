9020081825b1ef8e39122943a90e84d1b4d801e0
LL-Etiane <lukongleinyuyetiane@gmail.com>
2022-10-08 11:52:34 +0100

    login page ui designed and tailwind form added

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -2,7 +2,20 @@
 </script>
 
 <template>
-    <main>
-        <h1>Login page</h1>
+    <main class="h-screen grid place-items-center">
+        <div class="bg-white py-2 px-4 rounded-sm shadow-md">
+            <h1 class="my-2">Login page</h1>
+            <form action="">
+                <div class="form-group">
+                    <label for="email">Email</label>
+                    <input type="email" name="email" id="email" class="form-input w-full">
+                </div>
+                <div class="form-group">
+                    <label for="password">Password</label>
+                    <input type="password" name="password" id="password" class="w-full">
+                </div>
+                <input type="submit" value="Login" class="bg-primary p-2 my-2 rounded-sm text-white font-bold">
+            </form>
+        </div>
     </main>
 </template>
